13154a882b9e0580a1b76557be7f7ce9c21a9f16
ahmetozmtn <ahmethack13@gmail.com>
2024-02-10 20:09:25 +0300

    feat: added user query by id

--- routes/apiRouters.js
+++ routes/apiRouters.js
@@ -3,6 +3,24 @@ const router = express.Router();
 const dataUsers = require("../data/users.json");
 const dataMovies = require("../data/movies.json");
 
+router.use("/users/:id", (req, res) => {
+    if (req.method != "GET") {
+        res.status(405);
+        res.json({ error: "Method not allowed" });
+    } else {
+        const userId = req.params.id;
+        // console.log(userId);
+        const user = dataUsers.users.find((user) => user.id == userId);
+        if (user) {
+            res.status(200);
+            res.json(user);
+        } else {
+            res.status(404);
+            res.json({ error: "User not found" });
+        }
+    }
+});
+
 router.use("/users", (req, res) => {
     if (req.method != "GET") {
         res.status(405);
@@ -22,4 +40,5 @@ router.use("/movies", (req, res) => {
         res.json(dataMovies.movies);
     }
 });
+
 module.exports = router;
